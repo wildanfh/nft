e4fe19b9c56ff5975facee845a574cabb4dc488a
wildanfh <wildanf2005@gmail.com>
2026-03-15 07:51:27 +0100

    upload and use IPFS data

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,16 +5,21 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
+  mapping(uint256 => string) private sTokenIdToUri;
   uint256 private sTokenCounter;
+
   constructor() ERC721("Dogie", "DOG") {
     sTokenCounter = 0;
   }
 
-  function mintNft() public {
-
+  function mintNft(string memory tokenUri) public {
+    sTokenIdToUri[sTokenCounter] = tokenUri;
+    _safeMint(msg.sender, sTokenCounter);
+    sTokenCounter++;
   }
 
   function tokenURI(uint256 tokenId) public view override returns (string memory) {
-    return "";
+    return sTokenIdToUri[tokenId];
+    // "https://ipfs.tech/ipfs/QmYXqTGDFSpovvtYiuodfdUsfbEtP89Ss8KDbZUuVW4Pw8"
   }
 }
