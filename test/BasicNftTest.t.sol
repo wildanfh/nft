8aa25b0c23deb86f429929770e7349fc0d57e218
wildanfh <wildanf2005@gmail.com>
2026-03-16 11:29:08 +0100

    deploy nft on testnet

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -10,8 +10,7 @@ contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
     address public user = makeAddr("user");
-    string public constant PUG =
-        "ipfs://QmYXqTGDFSpovvtYiuodfdUsfbEtP89Ss8KDbZUuVW4Pw8";
+    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() public {
         deployer = new DeployBasicNft();
