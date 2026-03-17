27727aaa152629529d7b9a707f7d9f03ccc0a0c1
wildanfh <wildanf2005@gmail.com>
2026-03-17 23:36:58 +0100

    encoding svgs to be stored onchain

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,25 +2,62 @@
 pragma solidity ^0.8.19;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
-  uint256 private sTokenCounter;
-  string private sSadSvg;
-  string private sHappySvg;
+    uint256 private sTokenCounter;
+    string private sSadSvgImageUri;
+    string private sHappySvgImageUri;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private sTokenIdToMood;
+
     constructor(
-        string memory sadSvg,
-        string memory happySvg
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
     ) ERC721("MoodNft", "MN") {
         sTokenCounter = 0;
-        sSadSvg = sadSvg;
-        sHappySvg = happySvg;
+        sSadSvgImageUri = sadSvgImageUri;
+        sHappySvgImageUri = happySvgImageUri;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, sTokenCounter);
+        sTokenIdToMood[sTokenCounter] = Mood.HAPPY;
         sTokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI = sTokenIdToMood[tokenId] == Mood.HAPPY
+            ? sHappySvgImageUri
+            : sSadSvgImageUri;
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "',
+                                name(),
+                                '", "description": "An NFT that reflects the owner mood.", "attributes": [{"trait_type": "moodiness", "value": 100}], "image": "',
+                                imageURI,
+                                '" }'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
